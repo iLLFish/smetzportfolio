a8ccfd452ffa25c5070793135c43a672e09eed2e
Sascha Metz <metz@raum3.de>
2018-09-29 19:07:21 +0200

    Added Inital Links and Favicon

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,24 @@
  * raum3 gulpfile - Version Dec 2017 - Bootstrap4 ready -
  */
 
-const gulp          = require('gulp');
-const sass          = require('gulp-sass');
-const watch         = require('gulp-watch');
-const concat        = require('gulp-concat');
-const autoprefixer  = require('autoprefixer');
-const postcss       = require('gulp-postcss');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
+const concat = require('gulp-concat');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 const flexbugsfixes = require('postcss-flexbugs-fixes');
-const cleanCss      = require('gulp-clean-css');
-const livereload    = require('gulp-livereload');
-const uglify        = require('gulp-uglify');
-const babel 		= require('gulp-babel');
-const sourcemaps    = require('gulp-sourcemaps');
+const cleanCss = require('gulp-clean-css');
+const livereload = require('gulp-livereload');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 const processors = [
     flexbugsfixes,
     autoprefixer({
-        browsers: ['last 2 versions','> 0.1%']
+        browsers: ['last 2 versions', '> 0.1%']
     })
 ];
 
@@ -31,51 +31,50 @@ const paths = {
     php: '**/*.php',
     scripts: ['src/js/*.js'],
     plugins: ['node_modules/tiny-slider/dist/min/tiny-slider.js'],
-    css : []
+    css: []
 };
 
-gulp.task('sass',() => {
+gulp.task('sass', () => {
     gulp.src('src/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('styles.min.css'))
         .pipe(postcss(processors))
-        .pipe(cleanCss({compatibility: 'ie8'}))
+        .pipe(cleanCss({ compatibility: 'ie8' }))
         .pipe(gulp.dest('assets/css'))
 });
 
 gulp.task('compress', () => {
     gulp.src(paths.scripts)
-		.pipe(sourcemaps.init())
-		.pipe(babel({presets: ['@babel/env']}))
+        .pipe(sourcemaps.init())
+        .pipe(babel({ presets: ['@babel/env'] }))
         .pipe(concat('scripts.min.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('assets/js'))
 });
 
-gulp.task('plugins', () =>{
+gulp.task('plugins', () => {
     gulp.src(paths.plugins)
-    	// .pipe(sourcemaps.init())
-	    //.pipe(babel({presets: ['@babel/env']}))
+        // .pipe(sourcemaps.init())
+        //.pipe(babel({presets: ['@babel/env']}))
         .pipe(concat('plugins.min.js'))
-		//.pipe(uglify())
+        //.pipe(uglify())
         // .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('assets/js'));
     gulp.src(paths.css)
         .pipe(concat('plugins.css'))
         .pipe(postcss(processors))
-        .pipe(cleanCss({compatibility: 'ie8'}))
+        .pipe(cleanCss({ compatibility: 'ie8' }))
         .pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('watch', () => {
-    livereload({start: true});
+    livereload({ start: true });
     gulp.watch(paths.scss, ['sass']);
     gulp.watch(paths.scripts, ['compress']);
-    gulp.src(paths.php).pipe(watch(paths.php)).pipe(livereload());
-    gulp.src('assets/js/*.js').pipe(watch('assets/js/*.js')).pipe(livereload());
-    gulp.src('assets/css/*.css').pipe(watch('assets/css/*.css')).pipe(livereload());
+    gulp.src('assets/js/*.js').pipe(watch('assets/js/*.js'));
+    gulp.src('assets/css/*.css').pipe(watch('assets/css/*.css'));
 });
 
 gulp.task('default', ['sass', 'compress', 'plugins']);
-gulp.task('watch_with_default',['default','watch']);
+gulp.task('watch_with_default', ['default', 'watch']);
